74b98766fac1af99a304039adeb512b27fc892f8
feifeigd <feifeigd@21cn.com>
2023-05-26 09:38:14 +0800

    ServiceAccount

--- hello-client/main.go
+++ hello-client/main.go
@@ -8,7 +8,11 @@ import (
 	"path/filepath"
 
 	"d7kj.com/k8s"
+	apiAppv1 "k8s.io/api/apps/v1"
+	apiCorev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -32,8 +36,8 @@ func main() {
 	// ListNodes(clientset)
 	// ListNamespaces(clientset)
 	// ListDeployment(clientset)
-	ListService(clientset)
-
+	// ListService(clientset)
+	CreateDeployment(clientset)
 }
 
 func ListNodes(client *kubernetes.Clientset) {
@@ -73,6 +77,77 @@ func ListService(client *kubernetes.Clientset) {
 	fmt.Println(nodes)
 }
 
-func CreateDeployment(client *kubernetes.Clientset) {
-	// deployment := &apiAppv1.Deployment{}
+func CreateDeployment(client *kubernetes.Clientset) error {
+	var (
+		replicas                        int32 = 2
+		AutomountServiceAccountTokenYes       = true
+	)
+
+	deployment := &apiAppv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "k8s-test-stub",
+			Namespace: "testing",
+			Labels: map[string]string{
+				"app": "k8s-test-app",
+			},
+			Annotations: map[string]string{
+				"creator": "k8s-test-app",
+			},
+		},
+		Spec: apiAppv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "k8s-test-app",
+				},
+			},
+			Replicas: &replicas,
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "k8s-test-app",
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []apiCorev1.Container{
+						{
+							Name:  "nginx",
+							Image: "nginx:alpine",
+						},
+					},
+					RestartPolicy:                apiCorev1.RestartPolicyAlways,
+					DNSPolicy:                    apiCorev1.DNSClusterFirst,
+					NodeSelector:                 nil,
+					ServiceAccountName:           "",
+					AutomountServiceAccountToken: &AutomountServiceAccountTokenYes,
+				},
+			},
+			Strategy: apiAppv1.DeploymentStrategy{
+				Type: apiAppv1.RollingUpdateDeploymentStrategyType,
+				RollingUpdate: &apiAppv1.RollingUpdateDeployment{
+					MaxUnavailable: &intstr.IntOrString{
+						Type:   intstr.String,
+						IntVal: 0,
+						StrVal: "25%",
+					},
+					MaxSurge: &intstr.IntOrString{
+						Type:   intstr.String,
+						IntVal: 0,
+						StrVal: "25%",
+					},
+				},
+			},
+		},
+	}
+
+	dp, err := client.AppsV1().Deployments("testing").Create(context.Background(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		// return err
+	}
+	fmt.Println(dp)
+	fmt.Println(err)
+	return err
 }
