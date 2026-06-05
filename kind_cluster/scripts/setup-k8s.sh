3add6b493236e477f61d91ea4e7a7e1d07a3d6d0
feifeigd <feifeigd@21cn.com>
2026-06-06 01:51:08 +0800

    添加 Kubernetes 命名空间配置文件，并增强安装脚本以检查必要命令和查看集群资源

--- kind_cluster/scripts/setup-k8s.sh
+++ kind_cluster/scripts/setup-k8s.sh
@@ -8,18 +8,62 @@ log() {
     echo "[setup-k8s.sh] $*"
 }
 
+require_cmd() {
+    if ! command -v "$1" &> /dev/null; then
+        log "$1 未安装"
+        exit 1
+    fi
+}
+
 main() {
+    require_cmd docker
+    require_cmd kind
+    require_cmd kubectl
+    
     create_cluster
     view_cluster
     # delete_cluster
+    kubectl config use-context kind-${CLUSTER_NAME}
+
+    kubectl get nodes
+    log "节点查看完成"
+
+    kubectl get namespaces
+    log "命名空间查看完成"
+
+    kubectl get pods
+    log "Pod 查看完成"
+
+    kubectl get services
+    log "服务查看完成"
+
+    kubectl get deployments
+    log "部署查看完成"
+
+    kubectl get statefulsets
+    log "状态集查看完成"
+
+    kubectl get jobs
+    log "任务查看完成"
+    
+    kubectl get cronjobs
+    log "定时任务查看完成"
 }
 
 create_cluster() {
-    if kind get clusters | grep -q $CLUSTER_NAME; then
+    if kind get clusters | grep -qx "$CLUSTER_NAME"; then
         log "集群已存在"
         return
     fi
     log "创建集群"
+    # 使用宿主机的代理, IP 通过 ip addr show docker0 查看
+    PROXY_URL="http://172.17.0.1:20171"
+    export HTTP_PROXY=$PROXY_URL
+    export HTTPS_PROXY=$PROXY_URL
+    export NO_PROXY=localhost,127.0.0.1,.svc,.cluster.local,172.17.0.0/16,10.96.0.0/12,10.244.0.0/16
+    export http_proxy=$HTTP_PROXY
+    export https_proxy=$HTTPS_PROXY
+    export no_proxy=$NO_PROXY
     kind create cluster --config k8s/kind-config.yaml
     log "集群创建完成"
 }
